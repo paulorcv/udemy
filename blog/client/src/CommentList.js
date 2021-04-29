6ac1e5873716e4feacd3d25fed8c995a317b16cf
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-04-29 08:14:31 -0300

    Consumir queryservice somente uma vez

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,22 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-export default ({postId}) => {
-
-    const [comments, setComments ] = useState({});
-
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-        setComments(res.data);
-    };
-
-    useEffect( () => {
-        fetchComments();
-    }, []);
-
+export default (comments) => {
     const renderedComments = Object.values(comments).map(comment => {
         return <li  key={comment.id}>{comment.content}</li>;                
     })
-
     return <ul>{renderedComments}</ul>;
 };
